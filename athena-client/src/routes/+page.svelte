9f57b2b46dfc9236b18d7a781b8ac42c3cf831b4
AbooMinister25 <aboominister@gmail.com>
2023-06-13 14:36:29 -0400

    Added support for SCSS and set up nord

--- athena-client/src/routes/+page.svelte
+++ athena-client/src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+	@import 'node_modules/nord/src/sass/nord.scss';
+
+	h1 {
+		color: $nord11;
+	}
+</style>
