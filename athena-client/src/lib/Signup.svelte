a05360742c54a0372a21bfe4329af55edb28f906
Sai Sarvasiddi <saisarvasiddi146@gmail.com>
2023-06-14 16:15:08 -0400

    check

---
+++ athena-client/src/lib/Signup.svelte
@@ -0,0 +1,84 @@
+<title>Athena Signup</title>
+
+<style type="text/css">
+input[type="text"],[type="password"],[type="submit"],[type="email"]{
+	width: 20%;
+	height: 5%;
+	padding-top: 10px;
+	background-color: #5E81AC;
+	color: #88C0D0;
+	position: absolute;
+	left: 40%;
+	margin top:5px;
+	}
+::placeholder {
+  color: #88C0D0;
+  opacity: 1;
+}
+
+:-ms-input-placeholder {
+  color: #88C0D0;
+}
+
+::-ms-input-placeholder {
+  color: #88C0D0;
+}
+select{
+	width: 20.45%;
+	height: 5%;
+	padding-top: 0px;
+	background-color: #5E81AC;
+	color: #88C0D0;
+	position: absolute;
+	left: 40%;
+	margin top:0px;
+}
+input[type="radio"]{
+	position: absolute;
+	left: 40%;
+	padding-top: 10px;
+}
+label{
+	position: absolute;
+	left: 41.5%;
+
+}
+</style>
+</head>
+<body>
+<h style="top:0%;position:absolute;left:45%;font-size: 30px;">Athena</h>
+<h style="top:0%;position:absolute;left:45%;font-size: 30px;"></h>
+
+<form>
+	<input type="text" name="" placeholder="First name" required><br><br><br>
+	<input type="text" name="" placeholder="Last name"  required><br><br><br>
+	<input type="text" name="" placeholder="Username" required><br><br>
+	<br><input type="password" name="" placeholder="Password" required pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$"><br><br><br>
+	<input type="password" name="" placeholder="Re-enter Password" required pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$"><br><br><br>
+	<input type="email" name="" placeholder="Email" required><br><br><br><br>
+	<select required>
+		<option value="1"><13</option>
+		<option value="2">13-20</option>
+		<option value="3">20-40</option>
+		<option value="4">40-60</option>
+		<option value="5">60<</option>	
+	</select><br><br>
+	<br><a href=""><input type="submit" name="" value="Signup"></a><br><br><br>
+</form>
+<br><a href=""><input type="submit" name="" value="If you already have an account Login Here"></a>
+
+<label style="position: absolute;top: 0.5%;left: 60.5%;color:red;">*</label>
+<label style="position: absolute;top: 6%;left: 60.5%;color:red;">*</label>
+<label style="position: absolute;top: 11.8%;left:  60.5%;color:red;">*</label>
+<label style="position: absolute;top: 17.4%;left:  60.5%;color:red;">*</label>
+<label style="position: absolute;top: 22.8%;left:  60.5%;color:red;">*</label>
+<label style="position: absolute;top: 28.5%;left:  60.5%;color:red;">*</label>
+<label style="position: absolute;top: 28.5%;left:  60.5%;color:red;">*</label>
+
+<script>
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = #81A1C1;
+    ctx.fillRect(20, 20, 150, 100);
+</script>
