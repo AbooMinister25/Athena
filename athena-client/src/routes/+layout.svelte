8ca7e851a17636d91388799bf56b28bb0a9c585e
AbooMinister25 <aboominister@gmail.com>
2023-06-13 23:04:27 -0400

    Added fonts

---
+++ athena-client/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Nav from '$lib/Nav.svelte';
+</script>
+
+<Nav />
+<slot />
+
+<style lang="scss">
+	@import '../../static/styles/fonts.scss';
+
+	:global(body) {
+		font-family: $sans-serif;
+	}
+</style>
--- athena-client/src/routes/+page.svelte
+++ athena-client/src/routes/+page.svelte
@@ -1,9 +1,3 @@
-<script lang="ts">
-	import Nav from '$lib/Nav.svelte';
-</script>
-
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
-
-<Nav />
